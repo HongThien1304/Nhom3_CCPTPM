16ca7bd9a02cb8d7a7cb81d73f393d23dcbba027
TranMinhNhut <you@example.com>
2025-09-22 10:33:18 +0700

    Revert changes from 1dc5728..HEAD (exclude merges)

--- Nhom3_1/Form1.Designer.cs
+++ Nhom3_1/Form1.Designer.cs
@@ -30,63 +30,29 @@
         {
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.lblUsername = new System.Windows.Forms.Label();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(305, 98);
+            this.txtUsername.Location = new System.Drawing.Point(350, 214);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(200, 22);
             this.txtUsername.TabIndex = 8;
-            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(205, 104);
+            this.lblUsername.Location = new System.Drawing.Point(250, 217);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(92, 16);
+            this.lblUsername.Size = new System.Drawing.Size(98, 16);
             this.lblUsername.TabIndex = 7;
-            this.lblUsername.Text = "tên đăng nhập";
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(305, 145);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(200, 22);
-            this.txtPassword.TabIndex = 10;
-            this.txtPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(205, 148);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(61, 16);
-            this.lblPassword.TabIndex = 9;
-            this.lblPassword.Text = "mật khẩu";
-            this.lblPassword.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.Location = new System.Drawing.Point(343, 202);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(89, 33);
-            this.btnLogin.TabIndex = 11;
-            this.btnLogin.Text = "đăng nhập";
-            this.btnLogin.UseVisualStyleBackColor = true;
+            this.lblUsername.Text = "Tên đăng nhập";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.lblUsername);
             this.Name = "Form1";
@@ -100,9 +66,6 @@
 
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label lblUsername;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.Label lblPassword;
-        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- Nhom3_1/Form1.cs
+++ Nhom3_1/Form1.cs
@@ -16,20 +16,5 @@ namespace Nhom3_1
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtUsername_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
